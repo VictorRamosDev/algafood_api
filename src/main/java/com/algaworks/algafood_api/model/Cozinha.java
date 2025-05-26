29d70fb2124eb17d3188d5438efe1e9ce1fabe26
Victor Hugo <victorhugopb8@gmail.com>
2025-05-25 22:46:44 -0300

    3.6. Mapeando o id da entidade para autoincremento

--- src/main/java/com/algaworks/algafood_api/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood_api/model/Cozinha.java
@@ -1,19 +1,18 @@
 package com.algaworks.algafood_api.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tab_cozinha") //Opcional: se o nome da tabela for o mesmo da classe
 public class Cozinha {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nom_cozinha") //Opcional: Se o nome do atributo de classe for o mesmo da coluna da tabela
     private String nome;
 
     public Long getId() {
