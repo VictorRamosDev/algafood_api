6c01a34cca479c9ce2ade0ed48da8c5ad5185017
Victor Hugo <victorhugopb8@gmail.com>
2025-06-30 12:17:35 -0300

    5.8. Conhecendo os prefixos de query methods

---
+++ src/main/java/com/algaworks/algafood_api/api/TestController.java
@@ -0,0 +1,32 @@
+package com.algaworks.algafood_api.api;
+
+import com.algaworks.algafood_api.domain.model.Restaurante;
+import com.algaworks.algafood_api.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    public TestController(RestauranteRepository restauranteRepository) {
+        this.restauranteRepository = restauranteRepository;
+    }
+
+    @GetMapping("/restaurantes/top2-por-nome")
+    public List<Restaurante> buscaRestaurantesTop2PorNome(String nome) {
+        return restauranteRepository.findTop2ByNomeContaining(nome);
+    }
+
+    @GetMapping("/restaurantes/count-por-cozinha")
+    public int buscaRestaurantesPorCozinha(Long cozinhaId) {
+        return restauranteRepository.countByCozinhaId(cozinhaId);
+    }
+}
