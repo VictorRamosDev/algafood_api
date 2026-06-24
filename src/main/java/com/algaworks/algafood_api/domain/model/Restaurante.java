dcd53c7d7fa5288b452f3a622c1933d5edde3d0f
Victor Hugo <victorhugopb8@gmail.com>
2026-06-24 17:43:17 -0300

    6.4. Mapeando classes incorporáveis com @Embedded e @Embeddable

--- src/main/java/com/algaworks/algafood_api/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood_api/domain/model/Restaurante.java
@@ -32,6 +32,9 @@ public class Restaurante {
     @JoinColumn(name = "cozinha_id", nullable = false) // Annotation par setar nome da coluna (chave estrangeira) na tabela Restaurante. É opcional, pois o padrão é "<nome da entidade>_id"
     private Cozinha cozinha;
 
+    @Embedded
+    private Endereco endereco;
+
     @JsonIgnore
     @ManyToMany
     @JoinTable(
