07fb3a241754e277dbb5c6322bf949a2e51bb728
Victor Hugo <victorhugopb8@gmail.com>
2026-07-07 00:32:58 -0300

    6.7. Desafio: mapeando relacionamento muitos-para-um

---
+++ src/main/java/com/algaworks/algafood_api/domain/model/Produto.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood_api.domain.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Produto {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(nullable = false)
+    private Restaurante restaurante;
+}
