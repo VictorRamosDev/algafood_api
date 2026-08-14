8cc5f1c00b844ef82ea5e77d80612acb4865d8ae
Victor Hugo <victorhugopb8@gmail.com>
2026-08-14 17:16:39 -0300

    8.4. Estendendo ResponseStatusException

--- src/main/java/com/algaworks/algafood_api/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood_api/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,15 +1,19 @@
 package com.algaworks.algafood_api.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class EntidadeNaoEncontradaException extends RuntimeException {
+//@ResponseStatus(HttpStatus.NOT_FOUND) // Desnecessário, pois já é tratado no construtor da classe da exceção
+public class EntidadeNaoEncontradaException extends ResponseStatusException {
 
     private static final long serialVersionUID = 1L;
 
+    public EntidadeNaoEncontradaException(HttpStatus status, String message) {
+        super(status, message);
+    }
+
     public EntidadeNaoEncontradaException(String message) {
-        super(message);
+        this(HttpStatus.NOT_FOUND, message);
     }
 
 }
