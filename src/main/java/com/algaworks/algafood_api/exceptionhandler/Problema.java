ac65cda8cf747183d82f67f55feb824b492f2c61
Victor Hugo <victorhugopb8@gmail.com>
2026-08-18 12:14:29 -0300

    8.12. Tratando exceções em nível de controlador com @ExceptionHandler

---
+++ src/main/java/com/algaworks/algafood_api/exceptionhandler/Problema.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood_api.exceptionhandler;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class Problema {
+
+    private LocalDateTime dateTime;
+    private String message;
+}
