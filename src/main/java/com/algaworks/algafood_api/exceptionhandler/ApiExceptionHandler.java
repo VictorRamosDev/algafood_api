a4d4f2a8dd7c426bfca4054e1c837a1fb8849b72
Victor Hugo <victorhugopb8@gmail.com>
2026-08-27 19:23:00 -0300

    9.3. Desafio: tratando exception de violação de constraints de validação

--- src/main/java/com/algaworks/algafood_api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood_api/exceptionhandler/ApiExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -29,6 +30,16 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     public static final String MSG_ERRO_GENERICA_USUARIO_FINAL = "Ocorreu um erro interno inesperado no sistema. " +
             "Tente novamente e se o problema persistir, entre em contato com o administrador do sistema.";
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ProblemType problemType = ProblemType.DADOS_INVALIDOS;
+        String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
+
+        Problem problem = createProblemBuilder(status, problemType, detail, detail).build();
+
+        return handleExceptionInternal(e, problem, new HttpHeaders(), status, request);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleUncaught(Exception e, WebRequest request) {
